e0aed8bdb29b9bf1a3111d92a95d16d566a4c3ae
123jason689 <jason.melvin689@gmail.com>
2025-09-28 16:26:33 +0700

    Installed gh-pages extension and managed the deployment settings for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base:'/porto/',
+  base:'/porto/',
   plugins: [
     react(), 
     tailwindcss()
